83591e26d5109778dd3b3a74ab1d7065c92051ef
shahpankaj123 <aayanshah615@gmail.com>
2022-05-11 12:08:31 +0545

    java

---
+++ example2.java
@@ -0,0 +1,10 @@
+public class example2 {
+    public static void main(String[] arg)
+    {
+        for(int i=0;i<arg.length;i++)
+        {
+            System.out.println("you are in main function");
+            System.out.println(arg[i]);
+        }
+    }
+}
