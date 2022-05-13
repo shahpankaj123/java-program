5819706d47ceaf8b84257e8233ebf87a6d5137fc
shahpankaj123 <aayanshah615@gmail.com>
2022-05-13 22:42:57 +0545

    constructor concept

---
+++ example5.java
@@ -0,0 +1,9 @@
+package pack1;
+import pack2.teacher;
+public class example5 {
+    public static void main(String [] args)
+    {
+      teacher t1=new teacher();
+      t1.showdata();
+    }
+}
---
+++ teacher.java
@@ -0,0 +1,16 @@
+package pack2;
+public class teacher {
+    private String name;
+    private String dep;
+    public teacher()
+    {
+        name="manisha";
+        dep="BEIT";
+    }
+    public void showdata()
+    {
+        System.out.println("Name="+name);
+        System.out.println("dep="+dep);
+    }
+    
+}
